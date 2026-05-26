23e1b1f69518ebb8d1ae47ca14ec064dea24385b
SaiSnehasree <medasaisneha@gmail.com>
2026-05-26 13:36:58 +0530

    Initial Spring Boot setup with PostgreSQL connection

---
+++ src/main/java/com/sneha/wms/EnterpriseWmsApplication.java
@@ -0,0 +1,13 @@
+package com.sneha.wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseWmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseWmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sneha/wms/EnterpriseWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sneha.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseWmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
