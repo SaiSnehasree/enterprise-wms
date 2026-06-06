a9e7fc8cc711d391b6f15547227ca8676b6d50ee
SaiSnehasree <medasaisneha@gmail.com>
2026-06-06 12:52:10 +0530

    Developed product QR generation service with ZXing integration

--- src/main/java/com/sneha/wms/controller/ProductController.java
+++ src/main/java/com/sneha/wms/controller/ProductController.java
@@ -1,11 +1,18 @@
 package com.sneha.wms.controller;
 
+import com.google.zxing.WriterException;
 import com.sneha.wms.entity.Product;
+import com.sneha.wms.service.BarcodeService;
 import com.sneha.wms.service.ProductService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import com.sneha.wms.dto.ProductDTO;
+
 import jakarta.validation.Valid;
+
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -13,94 +20,195 @@ import java.util.List;
 public class ProductController {
 
     @Autowired
-    private ProductService productService;
+    private ProductService
+            productService;
 
-    // Add Product
+    @Autowired
+    private BarcodeService
+            barcodeService;
+
+    // ADD PRODUCT
     @PostMapping
     public Product addProduct(
             @Valid
-            @RequestBody Product product) {
+            @RequestBody Product product
+    ) {
 
         return productService
                 .addProduct(product);
     }
-    // Get All Products
+
+    // GET ALL PRODUCTS
     @GetMapping
-    public List<Product> getAllProducts() {
-        return productService.getAllProducts();
+    public List<Product>
+    getAllProducts() {
+
+        return productService
+                .getAllProducts();
     }
 
-    // Get Product By Id
+    // GET PRODUCT BY ID
     @GetMapping("/{id}")
-    public Product getProductById(@PathVariable Long id) {
-        return productService.getProductById(id);
+    public Product getProductById(
+            @PathVariable Long id
+    ) {
+
+        return productService
+                .getProductById(id);
+    }
+    @GetMapping(
+            value = "/qr/{id}",
+            produces = "image/png"
+    )
+    public byte[]
+    generateQRCode(
+            @PathVariable Long id
+    )
+            throws Exception {
+
+        Product product =
+                productService
+                        .getProductById(id);
+
+        String filePath =
+                barcodeService
+                        .generateQRCode(
+                                product.getSku()
+                        );
+
+        return java.nio.file.Files
+                .readAllBytes(
+                        java.nio.file.Paths
+                                .get(filePath)
+                );
     }
 
-    // Delete Product
+    // DELETE PRODUCT
     @DeleteMapping("/{id}")
-    public String deleteProduct(@PathVariable Long id) {
-        productService.deleteProduct(id);
-        return "Product deleted successfully!";
+    public String deleteProduct(
+            @PathVariable Long id
+    ) {
+
+        productService
+                .deleteProduct(id);
+
+        return
+                "Product deleted successfully!";
     }
 
     @GetMapping("/test")
     public String testApi() {
-        return "WMS Product API Working!";
+
+        return
+                "WMS Product API Working!";
     }
 
+    // ADD SAMPLE PRODUCT
     @GetMapping("/add-sample")
     public Product addSampleProduct() {
 
-        Product product = new Product();
+        Product product =
+                new Product();
 
-        product.setSku("SKU001");
-        product.setProductName("Laptop");
-        product.setDescription("Dell Gaming Laptop");
-        product.setPrice(75000.0);
-        product.setQuantity(10);
+        product.setSku(
+                "SKU001"
+        );
 
-        return productService.addProduct(product);
+        product.setProductName(
+                "Laptop"
+        );
+
+        product.setDescription(
+                "Dell Gaming Laptop"
+        );
+
+        product.setPrice(
+                75000.0
+        );
+
+        product.setQuantity(
+                10
+        );
+
+        return productService
+                .addProduct(product);
     }
 
+    // UPDATE PRODUCT
     @PutMapping("/{id}")
     public Product updateProduct(
             @PathVariable Long id,
 
             @Valid
-            @RequestBody Product product) {
+            @RequestBody Product product
+    ) {
 
         return productService
-                .updateProduct(id, product);
+                .updateProduct(
+                        id,
+                        product
+                );
     }
 
     @GetMapping("/update-sample")
-    public Product updateSampleProduct() {
+    public Product
+    updateSampleProduct() {
 
-        Product updatedProduct = new Product();
+        Product updatedProduct =
+                new Product();
 
-        updatedProduct.setSku("SKU001");
-        updatedProduct.setProductName("Dell Gaming Laptop Pro");
-        updatedProduct.setDescription("Updated Gaming Laptop");
-        updatedProduct.setPrice(90000.0);
-        updatedProduct.setQuantity(20);
+        updatedProduct.setSku(
+                "SKU001"
+        );
 
-        return productService.updateProduct(
-                1L, updatedProduct);
+        updatedProduct.setProductName(
+                "Dell Gaming Laptop Pro"
+        );
+
+        updatedProduct.setDescription(
+                "Updated Gaming Laptop"
+        );
+
+        updatedProduct.setPrice(
+                90000.0
+        );
+
+        updatedProduct.setQuantity(
+                20
+        );
+
+        return productService
+                .updateProduct(
+                        1L,
+                        updatedProduct
+                );
     }
+
+    // DTO API
     @GetMapping("/dto")
-    public List<ProductDTO> getAllProductDTOs() {
+    public List<ProductDTO>
+    getAllProductDTOs() {
 
         return productService
                 .getAllProductDTOs();
     }
+
+    // VALIDATION TEST
     @GetMapping("/validation-test")
     public Product validationTest() {
 
-        Product product = new Product();
+        Product product =
+                new Product();
 
         product.setProductName("");
-        product.setPrice(-100.0);
-        product.setQuantity(-5);
+
+        product.setPrice(
+                -100.0
+        );
+
+        product.setQuantity(
+                -5
+        );
 
         return productService
                 .addProduct(product);
--- src/main/java/com/sneha/wms/service/BarcodeService.java
+++ src/main/java/com/sneha/wms/service/BarcodeService.java
@@ -1,4 +1,82 @@
 package com.sneha.wms.service;
 
-public class BarcodeServic {
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+
+import com.google.zxing.client.j2se
+        .MatrixToImageWriter;
+
+import com.google.zxing.common
+        .BitMatrix;
+
+import com.google.zxing.qrcode
+        .QRCodeWriter;
+
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+@Service
+public class BarcodeService {
+
+    public String
+    generateQRCode(
+            String sku
+    )
+            throws WriterException,
+            IOException {
+
+        QRCodeWriter
+                qrCodeWriter =
+                new QRCodeWriter();
+
+        BitMatrix bitMatrix =
+                qrCodeWriter.encode(
+                        sku,
+                        BarcodeFormat.QR_CODE,
+                        300,
+                        300
+                );
+
+        // Create folder if not exists
+        Path folderPath =
+                FileSystems
+                        .getDefault()
+                        .getPath(
+                                "generated-qrcodes"
+                        );
+
+        if (!Files.exists(
+                folderPath
+        )) {
+
+            Files.createDirectories(
+                    folderPath
+            );
+        }
+
+        String filePath =
+                "generated-qrcodes/"
+                        + sku
+                        + ".png";
+
+        Path path =
+                FileSystems
+                        .getDefault()
+                        .getPath(
+                                filePath
+                        );
+
+        MatrixToImageWriter
+                .writeToPath(
+                        bitMatrix,
+                        "PNG",
+                        path
+                );
+
+        return filePath;
+    }
 }
